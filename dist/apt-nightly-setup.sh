a381a5acacd21507d5ae328259918a6522f23ae7
Jonne Haß <me@jhass.eu>
2015-12-06 17:08:06 +0100

    nightly is on https now

--- dist/apt-nightly-setup.sh
+++ dist/apt-nightly-setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 apt-key adv --keyserver keys.gnupg.net --recv-keys 09617FD37CC06B54
-echo "deb http://nightly.crystal-lang.org/apt crystal main" > /etc/apt/sources.list.d/crystal-nightly.list
+echo "deb https://nightly.crystal-lang.org/apt crystal main" > /etc/apt/sources.list.d/crystal-nightly.list
 apt-get update
--- dist/rpm-nightly-setup.sh
+++ dist/rpm-nightly-setup.sh
@@ -2,6 +2,6 @@
 cat > /etc/yum.repos.d/crystal-nightly.repo <<END
 [crystal]
 name = Crystal
-baseurl = http://nightly.crystal-lang.org/rpm/
+baseurl = https://nightly.crystal-lang.org/rpm/
 gpgcheck=0
 END
